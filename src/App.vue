17b06ad3ea1c10c0ac0370cee2ec9ab68e53f033
MiguelGuevara25 <miguel_g1789@hotmail.com>
2023-11-18 23:08:59 -0500

    page completed

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
-<script >
-
-import {defineComponent} from "vue";
+<script>
+import { defineComponent } from "vue";
 import ToolbarGrillaSection from "@/prestamos/pages/tus-planes/toolbar.component.vue";
 import IniciarPagoView from "@/prestamos/pages/iniciar-plan-pago/iniciar_plan_pago.component.vue";
 import HeaderView from "@/shared/components/header-section.vue";
+import Login from "./login/components/Login.vue";
+import Register from "./register/components/Register.vue";
 
 export default defineComponent({
-  components: {HeaderView, IniciarPagoView, ToolbarGrillaSection}
-})
+  components: {
+    HeaderView,
+    IniciarPagoView,
+    ToolbarGrillaSection,
+    Login,
+    Register,
+  },
+});
 </script>
 
 <template>
   <header-view></header-view>
-  <iniciar-pago-view></iniciar-pago-view>
+  <!-- <iniciar-pago-view></iniciar-pago-view> -->
+  <Register />
 </template>
 
-<style >
-
-</style>
+<style></style>
